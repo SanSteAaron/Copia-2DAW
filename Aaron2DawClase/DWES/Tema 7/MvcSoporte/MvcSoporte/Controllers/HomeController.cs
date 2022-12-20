597d1088487980ddd6d915d84fa1dc1d2090a2a2
SanSteAaron <SanSteAaron@gmail.com>
2022-12-20 20:50:00 +0100

    MisAvisosController DWES

--- Aaron2DawClase/DWES/Tema 7/MvcSoporte/MvcSoporte/Controllers/HomeController.cs
+++ Aaron2DawClase/DWES/Tema 7/MvcSoporte/MvcSoporte/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcSoporte.Data;
 using MvcSoporte.Models;
+using System.Data;
 using System.Diagnostics;
 
 namespace MvcSoporte.Controllers
@@ -7,14 +10,26 @@ namespace MvcSoporte.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcSoporteContexto _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MvcSoporteContexto context)
         {
             _logger = logger;
+            _context = context;
         }
-
         public IActionResult Index()
         {
+            // Busca el empleado correspondiente al usuario actual. Si existe, activa la
+            // vista (View) y en caso contrario, se redirige para crear el empleado.
+            string? emailUsuario = User.Identity.Name;
+            Empleado? empleado = _context.Empleados.Where(e => e.Email == emailUsuario)
+                                  .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+                User.IsInRole("Usuario") &&
+                empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
             return View();
         }
 
