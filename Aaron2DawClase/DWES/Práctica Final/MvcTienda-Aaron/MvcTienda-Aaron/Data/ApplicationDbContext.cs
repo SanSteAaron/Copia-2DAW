9ad5e6cd3f89df24f540f38d4cd3a1656177a758
SanSteAaron <SanSteAaron@gmail.com>
2023-01-09 19:13:47 +0100

    9/01/2023

---
+++ Aaron2DawClase/DWES/Práctica Final/MvcTienda-Aaron/MvcTienda-Aaron/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcTienda_Aaron.Models;
+
+namespace MvcTienda_Aaron.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MvcTienda_Aaron.Models.Cliente> Cliente { get; set; }
+    }
+}
