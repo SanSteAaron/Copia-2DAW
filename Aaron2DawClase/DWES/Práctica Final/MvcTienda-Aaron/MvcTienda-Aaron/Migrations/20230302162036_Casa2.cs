04266756fa73b3a0faa094ccb4aeaa28b7acf503
AarónSanchezStefanov <SanSteAaron@gmail.com>
2023-03-15 15:35:11 +0100

    15/03

---
+++ Aaron2DawClase/DWES/Práctica Final/MvcTienda-Aaron/MvcTienda-Aaron/Migrations/20230302162036_Casa2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcTienda_Aaron.Migrations
+{
+    public partial class Casa2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
